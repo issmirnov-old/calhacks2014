0aeaf0e2463afb866e5e3f7bc35953e8ce6fafe7
Ivan Smirnov <isgsmirnov@gmail.com>
2014-10-05 03:02:50 +0000

    about to do massive change

--- main.js
+++ main.js
@@ -52,9 +52,30 @@ app.get('/light', function (req, res) {
 app.get('/listen', function (req, res) {
     'use strict';
     console.log("requst: " + req)
+
+    // parses the request url
+    var theUrl = url.parse( req.url );
+
+    // gets the query part of the URL and parses it creating an object
+    var queryObj = queryString.parse( theUrl.query );
+
+    // queryObj will contain the data of the query as an object
+    // and jsonData will be a property of it
+    // so, using JSON.parse will parse the jsonData to create an object
+    var obj = JSON.parse( queryObj.jsonData );
+
+    // as the object is created, the live below will print "bar"
+    console.log( obj.foo );
+
+
     var phrase = querystring.parse(req)["phrase"];
     console.log("phrase:" + phrase)
     listen(phrase);
+
+
+
+
+
     res.send('sent ' + phrase + ' to wit');
 });
 
